3808481b45a243d43d332eea7b1263486014bdbf
Tim Peel <tim.peel@equalexperts.com>
2025-09-05 15:00:51 +0100

    Handle clearance requests

--- src/Api/Endpoints/SummaryResponse.cs
+++ src/Api/Endpoints/SummaryResponse.cs
@@ -4,5 +4,6 @@ namespace Defra.TradeImportsReportingApi.Api.Endpoints;
 
 public record SummaryResponse(
     [property: JsonPropertyName("releases")] ReleasesSummaryResponse Releases,
-    [property: JsonPropertyName("matches")] MatchesSummaryResponse Matches
+    [property: JsonPropertyName("matches")] MatchesSummaryResponse Matches,
+    [property: JsonPropertyName("clearanceRequests")] ClearanceRequestsSummaryResponse ClearanceRequests
 );
