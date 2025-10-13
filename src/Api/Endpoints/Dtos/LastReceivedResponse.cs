47036c3f5803e346fe9681919c990cb576c6e348
Thomas Anderson <t11omas1983@live.co.uk>
2025-10-13 07:15:43 +0100

    PR comment updates

--- src/Api/Endpoints/Dtos/LastReceivedResponse.cs
+++ src/Api/Endpoints/Dtos/LastReceivedResponse.cs
@@ -4,6 +4,6 @@ namespace Defra.TradeImportsReportingApi.Api.Endpoints.Dtos;
 
 public record LastReceivedResponse(
     [property: JsonPropertyName("finalisation")] LastMessageResponse? Finalisation,
-    [property: JsonPropertyName("request")] LastMessageResponse? Request,
-    [property: JsonPropertyName("notification")] LastMessageResponse? Notification
+    [property: JsonPropertyName("clearanceRequest")] LastMessageResponse? Request,
+    [property: JsonPropertyName("preNotification")] LastMessageResponse? Notification
 );
