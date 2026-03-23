033c2b3d1768fcf310d5cef527531761e696b22b
t11omas <t11omas1983@live.co.uk>
2026-03-23 09:24:13 +0000

    CDMS-1324 - DLQ count endpoint (#44)
    * Adopts shared SQS DLQ management library
    
    Replaces custom SQS Dead Letter Queue (DLQ) service and admin endpoints with functionality provided by the `Defra.TradeImports.SQS.Endpoints` package.
    
    This standardizes DLQ operations like redrive, message removal, and draining, promoting reusability and reducing custom code maintenance. Removes the previous custom SQS Dead Letter Service implementation, its associated endpoint mappings, and unit tests.
    
    Relates to CDMS-1324
    
    * Updated to use openapi and removed swapper
    
    * updated nuget packages
    
    * added admin tags
    
    * Update SQS Endpoints package version to 0.1.2

--- src/Api/OpenApi/ServiceCollectionExtensions.cs
+++ src/Api/OpenApi/ServiceCollectionExtensions.cs
@@ -1,6 +1,5 @@
 using System.Diagnostics.CodeAnalysis;
-using System.Reflection;
-using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi;
 
 namespace Defra.TradeImportsReportingApi.Api.OpenApi;
 
@@ -9,55 +8,29 @@ public static class ServiceCollectionExtensions
 {
     public static void AddOpenApi(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddEndpointsApiExplorer();
-        services.AddSwaggerGen(c =>
+        services.AddOpenApi(options =>
         {
-            c.AddServer(
-                new OpenApiServer { Url = "https://" + (configuration.GetValue<string>("OpenApi:Host") ?? "localhost") }
-            );
-            c.AddSecurityDefinition(
-                "Basic",
-                new OpenApiSecurityScheme
+            options.OpenApiVersion = OpenApiSpecVersion.OpenApi3_0;
+            options.AddDocumentTransformer(
+                (document, context, cancellationToken) =>
                 {
-                    Description = "RFC8725 Compliant JWT",
-                    In = ParameterLocation.Header,
-                    Name = "Authorization",
-                    Scheme = "Basic",
-                    Type = SecuritySchemeType.Http,
-                }
-            );
-            c.AddSecurityRequirement(
-                new OpenApiSecurityRequirement
-                {
+                    document.Info = new OpenApiInfo
                     {
-                        new OpenApiSecurityScheme
+                        Title = "Trade Imports Reporting API",
+                        Description = "TBC",
+                        Version = "v1",
+                        Contact = new OpenApiContact()
                         {
-                            Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "Basic" },
-                        },
-                        []
-                    },
-                }
-            );
-            c.IncludeXmlComments(Assembly.GetExecutingAssembly());
-            c.SupportNonNullableReferenceTypes();
-            c.UseAllOfToExtendReferenceSchemas();
-            c.SwaggerDoc(
-                "v1",
-                new OpenApiInfo
-                {
-                    Description = "TBC",
-                    Contact = new OpenApiContact
-                    {
-                        Email = "tbc@defra.gov.uk",
-                        Name = "DEFRA",
-                        Url = new Uri(
+                            Email = "tbc@defra.gov.uk",
+                            Name = "DEFRA",
+                            Url = new Uri(
 #pragma warning disable S1075
-                            "https://www.gov.uk/government/organisations/department-for-environment-food-rural-affairs"
+                                "https://www.gov.uk/government/organisations/department-for-environment-food-rural-affairs"
 #pragma warning restore S1075
-                        ),
-                    },
-                    Title = "Trade Imports Reporting API",
-                    Version = "v1",
+                            ),
+                        },
+                    };
+                    return Task.CompletedTask;
                 }
             );
         });
